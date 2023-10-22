726915bf9048ae0f1c85c89e690cf38a88aefde9
Pug <81167232+MerchantPug@users.noreply.github.com>
2023-10-22 22:40:29 +1100

    Remove IPlatformHelper from Services

--- common/src/main/java/dev/greenhouseteam/enchantmentconfig/platform/Services.java
+++ common/src/main/java/dev/greenhouseteam/enchantmentconfig/platform/Services.java
@@ -1,13 +1,10 @@
 package dev.greenhouseteam.enchantmentconfig.platform;
 
 import dev.greenhouseteam.enchantmentconfig.api.util.EnchantmentConfigUtil;
-import dev.greenhouseteam.enchantmentconfig.platform.services.IPlatformHelper;
 
 import java.util.ServiceLoader;
 
 public class Services {
-    public static final IPlatformHelper PLATFORM = load(IPlatformHelper.class);
-
     public static <T> T load(Class<T> clazz) {
 
         final T loadedService = ServiceLoader.load(clazz)
