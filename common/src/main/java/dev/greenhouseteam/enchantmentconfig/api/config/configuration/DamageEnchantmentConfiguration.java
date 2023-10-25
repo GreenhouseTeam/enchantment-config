f3088fa495cfa23a341ad3826a38ff3d208a8e62
Pug <81167232+MerchantPug@users.noreply.github.com>
2023-10-25 12:23:02 +1100

    Update DamageEnchantmentConfiguration.

--- common/src/main/java/dev/greenhouseteam/enchantmentconfig/api/config/configuration/DamageEnchantmentConfiguration.java
+++ common/src/main/java/dev/greenhouseteam/enchantmentconfig/api/config/configuration/DamageEnchantmentConfiguration.java
@@ -3,10 +3,10 @@ package dev.greenhouseteam.enchantmentconfig.api.config.configuration;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import dev.greenhouseteam.enchantmentconfig.api.codec.EnchantmentConfigCodecs;
+import dev.greenhouseteam.enchantmentconfig.api.util.EnchantmentConfigUtil;
 import dev.greenhouseteam.enchantmentconfig.api.util.MergeUtil;
 import net.minecraft.core.HolderSet;
 import net.minecraft.core.registries.Registries;
-import net.minecraft.util.Mth;
 import net.minecraft.world.entity.EntityType;
 
 import java.util.List;
@@ -15,42 +15,13 @@ import java.util.Optional;
 
 public record DamageEnchantmentConfiguration(Optional<List<HolderSet<EntityType<?>>>> affectedEntities,
                                              Map<Integer, Float> damage) implements EnchantmentConfiguration {
-
     public static Codec<DamageEnchantmentConfiguration> CODEC = RecordCodecBuilder.create(inst -> inst.group(
             EnchantmentConfigCodecs.defaultableCodec("affected_entities", Codec.list(EnchantmentConfigCodecs.tagOrElementCodec(Registries.ENTITY_TYPE))).forGetter(DamageEnchantmentConfiguration::affectedEntities),
-            EnchantmentConfigCodecs.levelToValueCodec("level", "damage", EnchantmentConfigCodecs.FLOAT).fieldOf("damage").forGetter(DamageEnchantmentConfiguration::damage)
+            EnchantmentConfigCodecs.rangeAllowedIntegerCodec("level", "damage", EnchantmentConfigCodecs.FLOAT).fieldOf("damage").forGetter(DamageEnchantmentConfiguration::damage)
     ).apply(inst, DamageEnchantmentConfiguration::new));
 
     public float getBonusDamageAmount(int level, float original) {
-        if (!damage.isEmpty()) {
-            if (damage.containsKey(level))
-                return damage.get(level);
-
-            if (damage.size() == 1) {
-                int key = damage.keySet().stream().findFirst().orElseThrow(() -> new NullPointerException("Tried to get first damage value when it was not present. This shouldn't happen."));
-                return damage.get(key) * ((float)level / key);
-            }
-
-            int upperBound = damage.keySet().stream().filter(i -> i > level).min((o1, o2) -> {
-                Integer i1 = Mth.abs(o1 - level);
-                Integer i2 = Mth.abs(o2 - level);
-
-                return i1.compareTo(i2);
-            }).orElseThrow();
-            int lowerBound = damage.keySet().stream().filter(i -> i < level).max((o1, o2) -> {
-                Integer i1 = Mth.abs(o1 - level);
-                Integer i2 = Mth.abs(o2 - level);
-
-                return i1.compareTo(i2);
-            }).orElseThrow();
-
-            float upperDamage = damage.get(upperBound);
-            float lowerDamage = damage.get(lowerBound);
-
-            return Mth.lerp(lowerDamage, upperDamage, (float) (level - lowerBound) / (upperBound - lowerBound));
-        }
-
-        return original;
+        return EnchantmentConfigUtil.getFloatFromLevel(level, original, this.damage());
     }
 
     @Override
--- common/src/main/java/dev/greenhouseteam/enchantmentconfig/api/util/EnchantmentConfigUtil.java
+++ common/src/main/java/dev/greenhouseteam/enchantmentconfig/api/util/EnchantmentConfigUtil.java
@@ -3,9 +3,11 @@ package dev.greenhouseteam.enchantmentconfig.api.util;
 import dev.greenhouseteam.enchantmentconfig.api.config.type.EnchantmentType;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.Mth;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Map;
 import java.util.Objects;
 
 public class EnchantmentConfigUtil {
@@ -27,6 +29,42 @@ public class EnchantmentConfigUtil {
 
     private static boolean checkCompatibility(EnchantmentType<?> enchantment, EnchantmentType<?> other) {
         return IEnchantmentConfigGetter.INSTANCE.getConfig(enchantment).getGlobalFields().incompatibilities().isPresent() ? IEnchantmentConfigGetter.INSTANCE.getConfig(enchantment).getGlobalFields().incompatibilities().get().stream().anyMatch(holders -> holders.contains(BuiltInRegistries.ENCHANTMENT.getHolderOrThrow(other.getEnchantment()))) : Objects.requireNonNull(BuiltInRegistries.ENCHANTMENT.get(enchantment.getEnchantment())).isCompatibleWith(Objects.requireNonNull(BuiltInRegistries.ENCHANTMENT.get(other.getEnchantment())));
+    }
+
+    public static float getFloatFromLevel(int level, float original, Map<Integer, Float> levelToValueMap) {
+        if (!levelToValueMap.isEmpty()) {
+            if (levelToValueMap.containsKey(level))
+                return levelToValueMap.get(level);
+
+            if (levelToValueMap.size() == 1) {
+                int key = levelToValueMap.keySet().stream().findFirst().orElseThrow(() -> new NullPointerException("Tried to get first damage value when it was not present. This shouldn't happen."));
+                return levelToValueMap.get(key) * ((float)level / key);
+            }
+
+            int upperBound = levelToValueMap.keySet().stream().filter(i -> i > level).min((o1, o2) -> {
+                Integer i1 = Mth.abs(o1 - level);
+                Integer i2 = Mth.abs(o2 - level);
+
+                return i1.compareTo(i2);
+            }).orElseThrow();
+            int lowerBound = levelToValueMap.keySet().stream().filter(i -> i < level).max((o1, o2) -> {
+                Integer i1 = Mth.abs(o1 - level);
+                Integer i2 = Mth.abs(o2 - level);
+
+                return i1.compareTo(i2);
+            }).orElseThrow();
+
+            float upperDamage = levelToValueMap.get(upperBound);
+            float lowerDamage = levelToValueMap.get(lowerBound);
+
+            if (lowerDamage == upperDamage) {
+                return lowerDamage;
+            }
+
+            return Mth.lerp(lowerDamage, upperDamage, (float) (level - lowerBound) / (upperBound - lowerBound));
+        }
+
+        return original;
     }
 
     public static ResourceLocation asResource(String path) {
