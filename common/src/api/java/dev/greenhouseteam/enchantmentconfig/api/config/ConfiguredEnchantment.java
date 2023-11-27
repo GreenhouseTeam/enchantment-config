329e35b9cd91c8a3af57242192d413b7fddc25f1
Pug <81167232+MerchantPug@users.noreply.github.com>
2023-11-28 06:52:51 +1100

    Fix ConfiguredEnchantment not having a globalPriority value.

--- common/src/api/java/dev/greenhouseteam/enchantmentconfig/api/config/ConfiguredEnchantment.java
+++ common/src/api/java/dev/greenhouseteam/enchantmentconfig/api/config/ConfiguredEnchantment.java
@@ -72,14 +72,14 @@ public class ConfiguredEnchantment<C extends EnchantmentConfiguration, T extends
         return (F) object;
     }
 
-    public ConfiguredEnchantment<C, T> merge(ConfiguredEnchantment<C, T> oldConfigured, Optional<ConfiguredEnchantment<C, T>> globalConfigured, int priority, int oldPriority) {
+    public ConfiguredEnchantment<C, T> merge(ConfiguredEnchantment<C, T> oldConfigured, Optional<ConfiguredEnchantment<C, T>> globalConfigured, int priority, int oldPriority, int globalPriority) {
         int newPriority = Math.max(priority, oldPriority);
 
         C configuration = (C) this.getConfiguration().mergeInternal(oldConfigured.getConfiguration(), priority, oldPriority);
 
-        GlobalEnchantmentFields globalFields = this.getGlobalFields().merge(oldConfigured.getGlobalFields(), globalConfigured.map(ConfiguredEnchantment::getGlobalFields), priority, oldPriority, 0);
+        GlobalEnchantmentFields globalFields = this.getGlobalFields().merge(oldConfigured.getGlobalFields(), globalConfigured.map(ConfiguredEnchantment::getGlobalFields), priority, oldPriority, globalPriority);
 
-        Map<String, Object> extraFields = MergeUtil.mergeMap(this.getExtraFields(), oldConfigured.getExtraFields(), Optional.empty(), priority, oldPriority, 0);
+        Map<String, Object> extraFields = MergeUtil.mergeMap(this.getExtraFields(), oldConfigured.getExtraFields(), priority, oldPriority);
 
         return new ConfiguredEnchantment<>(newPriority, this.getType(), configuration, globalFields, extraFields);
     }
