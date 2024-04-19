23cdb662d212b82fdafb1a96da206d7ee2f364c4
Pug <81167232+MerchantPug@users.noreply.github.com>
2024-04-20 09:03:05 +1000

    Add `unmodified` field to EnchantmentLevelVariable.

--- common/src/main/java/dev/greenhouseteam/enchantmentconfig/impl/variable/EnchantmentLevelVariable.java
+++ common/src/main/java/dev/greenhouseteam/enchantmentconfig/impl/variable/EnchantmentLevelVariable.java
@@ -1,5 +1,6 @@
 package dev.greenhouseteam.enchantmentconfig.impl.variable;
 
+import com.mojang.serialization.Codec;
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import dev.greenhouseteam.enchantmentconfig.api.config.variable.SingleTypedSerializer;
@@ -14,22 +15,27 @@ import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
 
-public record EnchantmentLevelVariable(Holder<Enchantment> otherEnchantment) implements SingleTypedVariable<Integer> {
+public record EnchantmentLevelVariable(Holder<Enchantment> otherEnchantment, boolean unmodified) implements SingleTypedVariable<Integer> {
     public static final ResourceLocation ID = EnchantmentConfigUtil.asResource("enchantment_level");
     public static final Serializer SERIALIZER = new Serializer();
     public static final MapCodec<EnchantmentLevelVariable> CODEC = RecordCodecBuilder.mapCodec(inst -> inst.group(
-            BuiltInRegistries.ENCHANTMENT.holderByNameCodec().fieldOf("enchantment").forGetter(EnchantmentLevelVariable::otherEnchantment)
+            BuiltInRegistries.ENCHANTMENT.holderByNameCodec().fieldOf("enchantment").forGetter(EnchantmentLevelVariable::otherEnchantment),
+            Codec.BOOL.optionalFieldOf("unmodified", false).forGetter(EnchantmentLevelVariable::unmodified)
     ).apply(inst, EnchantmentLevelVariable::new));
 
     @Override
     public Integer getValue(Enchantment enchantment, ItemStack stack, Integer original) {
-        if (otherEnchantment.isBound())
-            return stack.getEnchantments().getLevel(otherEnchantment.value());
+        if (otherEnchantment.isBound()) {
+            if (unmodified)
+                return stack.getEnchantments().getLevel(otherEnchantment.value());
+            return EnchantmentHelper.getItemEnchantmentLevel(otherEnchantment.value(), stack);
+        }
         return 0;
     }
 
-    public boolean canRootCondition() {
+    public boolean allowedInRootCondition() {
         return false;
     }
 
